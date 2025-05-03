e11aa6b016df86dabcb25db54f67744b7f1edec6
Patrick Reis Andrade <andrade.patrickreis@gmail.com>
2025-05-03 04:13:23 -0300

    Criando componentes e rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ScriptComponent } from './components/bootstrap/script/script.component';
+import { LinkComponent } from './components/bootstrap/link/link.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { IngredientsComponent } from './components/ingredients/ingredients.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LinkComponent, HeaderComponent, IngredientsComponent, HomeComponent , ScriptComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cmv';
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { IngredientsComponent } from './components/ingredients/ingredients.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+       path: "",
+       component: HomeComponent
+    },
+    {
+        path: "ingredients",
+        component: IngredientsComponent
+    }
+
+
+];
